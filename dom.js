0f4837efcf761e38e8c49ec1a6fc10b3e75454e1
Syed.Faraaz.Ali <Syed.Faraaz.Ali@skillassure.com>
2023-03-06 15:59:19 +0530

    Pushed 5

--- dom.js
+++ dom.js
@@ -4,11 +4,22 @@ console.log(li[1]);
 li[1].textContent = 'Hello 2';
 
 
-var item2 = document.querySelector('.list-group-item:nth-child(2)');
-item2.style.backgroundColor = 'green';
+var item = document.querySelector('.list-group-item:nth-child(1)');
+item.style.backgroundColor = 'green';
 
 var odd = document.querySelectorAll('li:nth-child(odd)');
 for(var i =0;i<odd.length;i++)
 {
     odd[i].style.backgroundColor= 'grey';
 }
+
+var newDivText = document.createTextNode("Hello World");
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+container.insertBefore(newDivText, h1);
+
+
+var items = document.querySelector('#items');
+
+items.insertBefore(newDivText, item);
