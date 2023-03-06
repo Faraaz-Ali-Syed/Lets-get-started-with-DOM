1943e60c1e430f48c6e62dd83ecef08e52ecc600
Syed.Faraaz.Ali <Syed.Faraaz.Ali@skillassure.com>
2023-03-06 15:28:09 +0530

    Pushed 4

--- dom.js
+++ dom.js
@@ -2,3 +2,13 @@ var li = document.getElementsByTagName('li');
 console.log(li);
 console.log(li[1]);
 li[1].textContent = 'Hello 2';
+
+
+var item2 = document.querySelector('.list-group-item:nth-child(2)');
+item2.style.backgroundColor = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i =0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor= 'grey';
+}
